e49935b1565ce10e1336b412e74b8333e64b83d3
김민수 MINSOO KIM <alstn113@gmail.com>
2023-10-05 19:22:50 +0900

    refactor: remove lombok

--- src/main/java/com/example/demo/domain/comment/Comment.java
+++ src/main/java/com/example/demo/domain/comment/Comment.java
@@ -1,5 +1,78 @@
 package com.example.demo.domain.comment;
 
+import com.example.demo.domain.post.Post;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import static java.util.Objects.hash;
+
+
+@Entity
 public class Comment {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private Post post;
+
+    protected Comment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public Comment(
+            final Long id,
+            final String content,
+            final LocalDateTime createdAt,
+            final Post post
+    ) {
+        this.id = id;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.post = post;
+    }
+
+    public Comment(
+            final String content,
+            final LocalDateTime createdAt,
+            final Post post
+    ) {
+        this(null, content, createdAt, post);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !getClass().equals(o.getClass())) return false;
+        final Comment comment = (Comment) o;
+        return Objects.equals(id, comment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return hash(id);
+    }
+
 }
--- src/main/java/com/example/demo/domain/post/Post.java
+++ src/main/java/com/example/demo/domain/post/Post.java
@@ -1,5 +1,28 @@
 package com.example.demo.domain.post;
 
+import com.example.demo.domain.comment.Comment;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 300)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+    private List<Comment> comments;
 
 }
