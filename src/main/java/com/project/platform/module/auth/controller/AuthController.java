26b0b03f2cca10d2cb5879006b680655ffc8dffb
김민수 MINSOO KIM <alstn113@gmail.com>
2023-10-24 10:58:44 +0900

    feat: cookie util

--- src/main/java/com/project/platform/module/auth/controller/AuthController.java
+++ src/main/java/com/project/platform/module/auth/controller/AuthController.java
@@ -7,11 +7,10 @@ import com.project.platform.module.auth.dto.AccessTokenResponse;
 import com.project.platform.module.auth.dto.LoginRequest;
 import com.project.platform.module.auth.dto.SignupRequest;
 import com.project.platform.module.auth.service.AuthService;
+import com.project.platform.module.auth.util.CookieUtil;
 import com.project.platform.module.member.domain.Member;
 import com.project.platform.module.member.dto.MemberResponse;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,10 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-
     private final AuthService authService;
-    private static final String REFRESH_TOKEN = "refresh-token";
-    private static final int COOKIE_AGE_SECONDS = 604800;
 
     public AuthController(final AuthService authService) {
         this.authService = authService;
@@ -35,41 +31,20 @@ public class AuthController {
     public ResponseEntity<MemberResponse> signup(@RequestBody final SignupRequest signupRequest) {
         Member newMember = authService.signup(signupRequest);
         return ResponseEntity.ok(MemberResponse.fromMember(newMember));
-
     }
 
     @PostMapping("/login")
     public ResponseEntity<AccessTokenResponse> login(@RequestBody final LoginRequest loginRequest,
                                                      final HttpServletResponse response) {
         JwtTokens jwtTokens = authService.login(loginRequest);
-        setRefreshTokenCookie(response, jwtTokens.refreshToken());
+        CookieUtil.setRefreshTokenCookie(response, jwtTokens.refreshToken());
         return ResponseEntity.status(CREATED).body(new AccessTokenResponse(jwtTokens.accessToken()));
     }
 
-    private void setRefreshTokenCookie(final HttpServletResponse response, final String refreshToken) {
-        final ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN, refreshToken)
-                .maxAge(COOKIE_AGE_SECONDS)
-                .sameSite("None")
-                .secure(true)
-                .httpOnly(true)
-                .path("/")
-                .build();
-        response.addHeader("Set-Cookie", cookie.toString());
-    }
 
     @DeleteMapping("/logout")
     public ResponseEntity<Void> logout(final HttpServletResponse response) {
-        clearTokenCookie(response);
-        return ResponseEntity.noContent().build();
-
+        CookieUtil.clearRefreshTokenCookie(response);
+        return ResponseEntity.ok().build();
     }
-
-    private void clearTokenCookie(final HttpServletResponse response) {
-        final Cookie cookie = new Cookie(REFRESH_TOKEN, null);
-        cookie.setMaxAge(0);
-        cookie.setPath("/");
-
-        response.addCookie(cookie);
-    }
-
 }
