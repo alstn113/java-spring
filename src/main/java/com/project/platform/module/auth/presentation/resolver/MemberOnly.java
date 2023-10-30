87f323c279ef1e9a426165a5021be223a244a4e9
김민수 MINSOO KIM <alstn113@gmail.com>
2023-10-30 21:51:48 +0900

    feat: get me impl

--- src/main/java/com/project/platform/module/auth/presentation/resolver/MemberOnly.java
+++ src/main/java/com/project/platform/module/auth/presentation/resolver/MemberOnly.java
@@ -4,11 +4,9 @@ package com.project.platform.module.auth.presentation.resolver;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import io.swagger.v3.oas.annotations.Hidden;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Hidden
 @Target(METHOD)
 @Retention(RUNTIME)
 public @interface MemberOnly {
--- src/main/java/com/project/platform/module/member/presentation/MemberController.java
+++ src/main/java/com/project/platform/module/member/presentation/MemberController.java
@@ -1,5 +1,8 @@
 package com.project.platform.module.member.presentation;
 
+import com.project.platform.module.auth.domain.Accessor;
+import com.project.platform.module.auth.presentation.resolver.Auth;
+import com.project.platform.module.auth.presentation.resolver.MemberOnly;
 import com.project.platform.module.member.application.MemberService;
 import com.project.platform.module.member.domain.Member;
 import com.project.platform.module.member.dto.MemberResponse;
@@ -18,9 +21,17 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/me")
+    @MemberOnly
+    public ResponseEntity<MemberResponse> getMemberById(@Auth final Accessor accessor) {
+        final Member member = memberService.getMemberByIdOrElseThrow(accessor.getMemberId());
+        return ResponseEntity.ok(MemberResponse.fromMember(member));
+    }
+
     @GetMapping("/{memberId}")
     public ResponseEntity<MemberResponse> getMemberById(@PathVariable final Long memberId) {
         final Member member = memberService.getMemberByIdOrElseThrow(memberId);
         return ResponseEntity.ok(MemberResponse.fromMember(member));
     }
+
 }
