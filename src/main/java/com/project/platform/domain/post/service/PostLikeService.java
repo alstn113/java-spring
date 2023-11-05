23fe33a19207b7f672b9b900599a05259942a8f2
김민수 MINSOO KIM <alstn113@gmail.com>
2023-11-06 00:12:25 +0900

    refactor: like service 수정

--- src/main/java/com/project/platform/domain/post/service/PostLikeService.java
+++ src/main/java/com/project/platform/domain/post/service/PostLikeService.java
@@ -1,8 +1,6 @@
 package com.project.platform.domain.post.service;
 
 import com.project.platform.domain.auth.domain.Accessor;
-import com.project.platform.domain.member.domain.Member;
-import com.project.platform.domain.member.service.MemberService;
 import com.project.platform.domain.post.domain.Post;
 import com.project.platform.domain.post.domain.PostLike;
 import com.project.platform.domain.post.domain.repository.PostLikeRepository;
@@ -13,38 +11,34 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PostLikeService {
-    private final MemberService memberService;
     private final PostRepository postRepository;
     private final PostLikeRepository postLikeRepository;
 
-    public PostLikeService(final MemberService memberService, final PostRepository postRepository,
+    public PostLikeService(final PostRepository postRepository,
                            final PostLikeRepository postLikeRepository) {
-        this.memberService = memberService;
         this.postRepository = postRepository;
         this.postLikeRepository = postLikeRepository;
     }
 
     public void likePost(Long postId, Accessor accessor) {
-        final Member member = memberService.getMemberByIdOrElseThrow(accessor.getMemberId());
         final Post post = postRepository.findById(postId).orElseThrow(
                 () -> new NotFoundException(ErrorCode.POST_NOT_FOUND)
         );
         final boolean isAlreadyLiked = postLikeRepository.existsByPostIdAndMemberId(post.getId(),
-                member.getId());
+                accessor.getMemberId());
         if (!isAlreadyLiked) {
-            postLikeRepository.save(new PostLike(post.getId(), member.getId()));
+            postLikeRepository.save(new PostLike(post.getId(), accessor.getMemberId()));
         }
     }
 
     public void unlikePost(Long postId, Accessor accessor) {
-        final Member member = memberService.getMemberByIdOrElseThrow(accessor.getMemberId());
         final Post post = postRepository.findById(postId).orElseThrow(
                 () -> new NotFoundException(ErrorCode.POST_NOT_FOUND)
         );
         final boolean isAlreadyLiked = postLikeRepository.existsByPostIdAndMemberId(post.getId(),
-                member.getId());
+                accessor.getMemberId());
         if (isAlreadyLiked) {
-            postLikeRepository.deleteByPostIdAndMemberId(post.getId(), member.getId());
+            postLikeRepository.deleteByPostIdAndMemberId(post.getId(), accessor.getMemberId());
         }
     }
 }
